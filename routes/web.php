edf0e12219806dfb0ddb4e7ac390081879e3d5ac
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-07-02 12:29:04 -0300

    probando

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 });
+
+
+Route::get('/tienda', function () {
+    return view('tienda/contacto');
+});

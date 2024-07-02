16531dddf9ee77bb50fad26d2be7057e95f846a7
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-07-02 16:49:57 -0300

    Inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,6 +3,148 @@
 
 @section('content')
 
-  <h1>fheiwhowefi</h1>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://fakeimg.pl/800x300/" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://fakeimg.pl/800x301/" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://fakeimg.pl/800x302/" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!--Container Nuevo y Ofertas-->
+  <div class="container containerMasNuevo">
+    <div class="row columnalomasnuevo">
+
+    <h1 class="tituloNuevo col-auto">Lo Mas Nuevo</h1>
+    <div class="linea col"></div>
+
+    </div>
+
+    <div class="contenedorcards row container">
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+    </div>
+
+
+
+
+    <div class="container containerOferta">
+    <div class="row columnaOferta">
+
+    <h1 class="tituloOferta col-auto">Ofertas</h1>
+    <div class="linea col"></div>
+
+    </div>
+
+    <div class="contenedorcards row container">
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+      <div class="card cardInicio col-auto" style="width: 18rem;">
+      <img class="card-img-top" src="https://fakeimg.pl/250x250/" alt="Card image cap">
+      <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="container">
+      <p>5000</p>
+      </div>
+      </div>
+
+    </div>
+
+    </div>
+
+  </div>
+
+  <div class="metodosPago">
+    <div class="container containermetodospago">
+  <img width="48" height="48" src="https://img.icons8.com/color/48/mercado-pago.png" alt="mercado-pago"/>
+  <img width="48" height="48" src="https://img.icons8.com/color/48/visa.png" alt="visa"/>
+  <img width="48" height="48" src="https://img.icons8.com/color/48/mastercard-logo.png" alt="mastercard-logo"/>
+  <img width="48" height="48" src="https://img.icons8.com/color/48/american-express-squared.png" alt="american-express-squared"/>
+  <img width="48" height="48" src="https://img.icons8.com/color/48/paypal.png" alt="paypal"/>
+  </div>
+  </div>
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{asset ('css/style.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
@@ -11,7 +13,7 @@
     
 
 <header>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark  navheader">
   <a class="navbar-brand" href="#">Nombre</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -42,30 +44,30 @@
     </main>
 
 
-<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+<footer class="text-center text-lg-start  text-muted footer">
   <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
     <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
+      <span>Siguenos en Nuestras Redes</span>
     </div>
 
     <div>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
+        <i class="bi bi-facebook"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
+        <i class="bi bi-twitter-x"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
+        <i class="bi bi-google"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
+        <i class="bi bi-instagram"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
+         <i class="bi bi-linkedin"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
+       <i class="bi bi-github"></i>
       </a>
     </div>
   </section>
@@ -75,59 +77,37 @@
       <!-- Grid row -->
       <div class="row mt-3">
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
-          </h6>
+          <p class="text-uppercase fw-bold mb-4">Nombre</p>
           <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut ex elementum, molestie sem sit amet, congue mauris. Aliquam posuere.
           </p>
         </div>
 
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Products
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Angular</a>
+          <p class="text-uppercase fw-bold mb-4">
+            Productos
           </p>
           <p>
-            <a href="#!" class="text-reset">React</a>
+            <a href="#!" class="text-reset">Incio</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Vue</a>
+            <a href="#!" class="text-reset">Nosotros</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Laravel</a>
-          </p>
-        </div>
-
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Pricing</a>
+            <a href="#!" class="text-reset">Contacto</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Settings</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Orders</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Help</a>
+            <a href="#!" class="text-reset">Productos</a>
           </p>
         </div>
 
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p class="text-uppercase fw-bold mb-4">Contacto</p>
+          <p><i class="fas fa-home me-3"></i> CABA, Buenos Aires</p>
           <p>
             <i class="fas fa-envelope me-3"></i>
-            info@example.com
+            nombre@example.com
           </p>
           <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
           <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
@@ -136,9 +116,8 @@
     </div>
   </section>
 
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2021 Copyright:
-    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  <div class="text-center p-4"  >
+    © Copyright
   </div>
 </footer>
 

91253b57eb7368a9189f0988ddb038d8df62c769
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-07-26 13:06:43 -0300

    se muestra bien el botin xd

--- resources/views/admin/botines/show.blade.php
+++ resources/views/admin/botines/show.blade.php
@@ -1,85 +1,72 @@
 @extends("layouts.admin")
 
-
 @section('content')
 
-
 <div class="container">
-        <h1>Botin {{ $botin->nombre}}</h1>
+    <h1 class="my-4">Botín {{ $botin->nombre }}</h1>
 
-        <div class="Botin-marca">
-            <p>Marca: {{ $botin->tipomarca->nombre }}</p>
+    <div class="row">
+
+        <div class="col-md-6">
+            <div class="mb-3">
+                <h4>Marca</h4>
+                <p>{{ $botin->tipomarca->nombre }}</p>
+            </div>
+
+            <div class="mb-3">
+                <h4>Precio</h4>
+                <p>${{ number_format($botin->precio, 2) }}</p>
+            </div>
+
+            <div class="mb-3">
+                <h4>Nombre</h4>
+                <p>{{ $botin->nombre }}</p>
+            </div>
+
+            <div class="mb-3">
+                <h4>Descripción</h4>
+                <p>{{ $botin->Descripcion }}</p>
+            </div>
         </div>
 
-
-        <div class="Botin-precio">
-            <p>Precio: {{ $botin->precio }}</p>
+        <div class="col-md-6">
+            <div class="mb-3">
+                @foreach($botin->imagenes as $imagen)
+                    <div class="mb-2">
+                        <img src="{{ asset('storage/' . $imagen->url_img) }}" alt="Imagen de {{ $botin->nombre }}" class="img-fluid">
+                    </div>
+                @endforeach
+            </div>
         </div>
+    </div>
 
-        <div class="Botin-dorsal">
-            <p>Dorsal: {{ $botin->nombre }}</p>
-        </div>
+    <div class="my-4">
+        <h3>Stock disponible</h3>
+        @if ($stockcalzado->isEmpty())
+            <p>No hay stock disponible.</p>
+        @else
+            <ul class="list-group">
+                @foreach ($stockcalzado as $stock)
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        {{ $stock->talleCalzado->nombre_talle }}
+                        <span class="badge bg-primary rounded-pill">{{ $stock->cantidad }} unidades</span>
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
 
-        <div class="Botin-descripcion">
-            <p>Descripción: {{ $botin->Descripcion}}</p>
-        </div>
+    <div class="mb-3">
+        <a href="{{route('botines.index')}}" class="btn btn-primary">Volver</a>
+        <a href="{{route('botines.edit', $botin)}}" class="btn btn-warning">Editar</a>
+        <a href="{{ route('botines.stock.create', $botin) }}" class="btn btn-secondary">Agregar Stock</a>
 
-        <div class="Botin-imagen">
-            @foreach ($botin->imagenes as $imagen)
-                <img src="{{ asset('storage/' . $imagen->url_img) }}" alt="Imagen del Botin">
-            @endforeach
-        </div>
-
-
-        <div class="my-4">
-            <h3>Stock disponible</h3>
-            @if ($stockcalzado->isEmpty())
-                <p>No hay stock disponible.</p>
-            @else
-                <ul class="list-group">
-                    @foreach ($stockcalzado as $stock)
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            {{ $stock->talleCalzado->nombre_talle }}
-                            <span class="badge bg-primary rounded-pill">{{ $stock->cantidad }} unidades</span>
-                        </li>
-                    @endforeach
-                </ul>
-            @endif
-        </div>
-
-
-
-
-        <div class="mb-3">
-            <a href="{{route('botines.index')}}" class="btn btn-primary">Volver</a>
-            <a href="{{route('botines.edit',$botin)}}" class="btn btn-primary">Editar</a>
-            <a href="{{ route('botines.stock.create', $botin) }}" class="btn btn-secondary">Agregar Stock</a>
-
-            <form action="{{route('botines.destroy',$botin)}}"" method="POST" class="d-inline">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger"> Eliminar </button>
-            </form>
-
-        </div>
+        <form action="{{ route('botines.destroy', $botin) }}" method="POST" class="d-inline">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Eliminar</button>
+        </form>
+    </div>
 </div>
 
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
